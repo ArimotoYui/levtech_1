8a29ad37268e96e6609b1b0ed113a134ae1ba574
ArimotoYui <akasatana05280708@gmail.com>
2023-07-13 18:49:22 +0900

    ブログ投稿作成・保存・エラー

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,13 +10,14 @@
         <h1>Blog Name</h1>
        
         <div class='posts'>
- 
+             <a href='/posts/create'>create</a>
             @foreach ($posts as $post)
                 <div class='post'>
                     <h2 class='title'>
                         <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                     </h2>
                     <p class='body'>{{ $post->body }}</p>
+                    
                 </div>
             @endforeach
         </div>
